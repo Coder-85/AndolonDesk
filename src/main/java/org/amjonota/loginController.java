f34574442f8e7c6d2a8cf3e4b9cdc921bdff809d
Sifat Al Islam <sifat.a.5532@gmail.com>
2026-02-13 14:55:21 +0600

    update: login and register page design done

--- src/main/java/org/amjonota/loginController.java
+++ src/main/java/org/amjonota/loginController.java
@@ -1,8 +1,24 @@
 package org.amjonota;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class loginController {
+
+    @FXML
+    private ImageView loginImg;
+    @FXML
+    private HBox imgContainer;
+
+    @FXML
+    public void initialize() {
+
+        loginImg.fitWidthProperty().bind(
+                imgContainer.widthProperty()
+        );
+
+    }
 
     @FXML
     public void loginBtn(){
