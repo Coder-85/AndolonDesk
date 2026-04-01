474c75719041b74182c103bbff4370b916be6f02
Sifat Al Islam <sifat.a.5532@gmail.com>
2026-04-01 19:15:38 +0600

    update: profile page protest loading

--- src/main/java/org/amjonota/ProfileController.java
+++ src/main/java/org/amjonota/ProfileController.java
@@ -1,20 +1,38 @@
 package org.amjonota;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.amjonota.auth.AuthService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.amjonota.model.ProtestItem;
 import org.amjonota.model.User;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileController {
     @FXML private Label profileName;
     @FXML private Label profileEmail;
     @FXML private Label profileDob;
     @FXML private Label profileJoined;
+    @FXML private VBox postList;
 
     @FXML
     public void initialize() {
@@ -25,6 +43,136 @@ public class ProfileController {
         profileEmail.setText(user.getEmail());
         profileDob.setText(user.getDateOfBirth() != null ? user.getDateOfBirth() : "N/A");
         profileJoined.setText(user.getCreatedAt() != null ? user.getCreatedAt() : "N/A");
+
+        try {
+            for (ProtestItem item : loadAllProtests()) {
+                postList.getChildren().add(buildCard(item));
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("Failed to load protests: " + e.getMessage());
+        }
+    }
+
+
+    private List<ProtestItem> loadAllProtests() throws SQLException {
+        List<ProtestItem> items = new ArrayList<ProtestItem>();
+
+        Connection conn = DatabaseManager.getInstance().getConnection();
+        String sql = "SELECT p.*, u.name AS author_name, (SELECT COUNT(*) FROM user_bookmarks ub WHERE ub.protest_id = p.id) AS bookmarked_count FROM protests p INNER JOIN users u ON u.id = p.author_id WHERE p.author_id = ?  ORDER BY p.posted_date DESC";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, Session.getCurrentUser().getId());
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                ProtestItem item = new ProtestItem(rs.getString("author_name"), rs.getInt("author_id"),rs.getString("posted_date"), rs.getString("title"), rs.getString("event_date"), rs.getString("summary"), rs.getString("description"), rs.getString("category"), rs.getInt("member_count"), rs.getInt("bookmarked_count"));
+                item.setId(rs.getInt("id"));
+                items.add(item);
+            }
+        }
+
+        return items;
+    }
+
+
+    private HBox buildCard(ProtestItem item) {
+        Label author = new Label(item.getAuthor());
+        author.setCursor(Cursor.HAND);
+        author.setOnMouseClicked(e -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(App.class.getResource("user_profile.fxml"));
+                Parent root = loader.load();
+
+                UserProfileController controller = loader.getController();
+                controller.setUserID(item.getAuthorID());
+
+                Stage stage = (Stage) author.getScene().getWindow();
+                stage.getScene().setRoot(root);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        author.getStyleClass().add("author");
+        Label postedDate = new Label("Posted: " + item.getPostedDate());
+        postedDate.getStyleClass().add("date");
+        VBox.setMargin(postedDate, new Insets(0, 0, 10, 0));
+
+        HBox topRow = new HBox();
+
+        VBox left = new VBox(author, postedDate);
+
+        Button editBtn = new Button("Edit");
+        editBtn.getStyleClass().addAll("btn", "btn-warning");
+
+        editBtn.setOnAction(e -> {
+            try {
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        VBox right = new VBox(editBtn);
+        right.setAlignment(Pos.TOP_RIGHT);
+        HBox.setHgrow(right, Priority.ALWAYS);
+
+        topRow.getChildren().addAll(left, right);
+
+        Label title = new Label(item.getTitle());
+        title.getStyleClass().add("title");
+        title.setWrapText(true);
+        Label eventDate = new Label("Event Date: " + item.getEventDate());
+        eventDate.getStyleClass().add("event");
+        VBox.setMargin(eventDate, new Insets(0, 0, 10, 0));
+        Label summary = new Label(item.getSummary());
+        summary.setWrapText(true);
+        VBox.setMargin(summary, new Insets(0, 0, 15, 0));
+        Button viewBtn = new Button("View Details");
+        viewBtn.getStyleClass().addAll("btn", "btn-primary");
+        viewBtn.setOnAction(e -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(App.class.getResource("andolon_details.fxml"));
+                Parent root = loader.load();
+
+                AndolonDetailsController controller = loader.getController();
+                controller.setPostID(item.getId());
+
+                Stage stage = (Stage) viewBtn.getScene().getWindow();
+                stage.getScene().setRoot(root);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        viewBtn.setMnemonicParsing(false);
+
+        VBox card = new VBox(topRow, title, eventDate, summary, viewBtn);
+        card.getStyleClass().add("post-card");
+        card.setPadding(new Insets(10));
+        card.setMaxHeight(VBox.USE_PREF_SIZE);
+        card.setMinHeight(VBox.USE_PREF_SIZE);
+        card.setMinWidth(VBox.USE_PREF_SIZE);
+        card.setPrefHeight(210);
+        card.setPrefWidth(497);
+        HBox.setHgrow(card, Priority.ALWAYS);
+        HBox.setMargin(card, new Insets(0, 0, 20, 0));
+        HBox wrapper = new HBox(card);
+        wrapper.getStyleClass().add("post-box-parent");
+        wrapper.setMaxHeight(HBox.USE_PREF_SIZE);
+        wrapper.setMinHeight(HBox.USE_PREF_SIZE);
+        wrapper.setMaxWidth(800);
+        wrapper.setPrefHeight(210);
+        wrapper.setPrefWidth(200);
+        wrapper.setPadding(new Insets(0, 0, 0, 5));
+
+        return wrapper;
     }
 
     @FXML
